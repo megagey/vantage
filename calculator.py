59e160cef35248bface5084fddc2902527f107f1
wened001 <wened001@mymail.unisa.edu.au>
2023-04-07 17:01:48 +0930

    Menu outline

--- calculator.py
+++ calculator.py
@@ -7,3 +7,36 @@ metalScrapPrice = 1251
 plasticPrice = 1251
 steelPrice = 1668
 rubberPrice = 834
+
+aluminiumStock = 5000
+copperStock = 5000
+fibreStock = 5000
+glassStock = 5000
+ironStock = 5000
+metalScrapStock = 5000
+plasticStock = 5000
+steelStock = 5000
+rubberStock = 5000
+
+menuText = f""
+
+def menu():
+    print(20 * "-", "Vantage Tech and Hardware", 20 * "-")
+    print("1 | Sell Item")
+    print("2 | Check Stock")
+    print("3 | Check Pricing")
+    print("3 | Exit")
+    return int(input("Please type 1, 2, 3 or 4"))
+
+choice = 0
+while True:
+    choice = menu()
+    if choice == 1:
+        print("Sell Item")
+    elif choice == 2:
+        print("Check Stock")
+    elif choice == 3:
+        print("Check Pricing")
+    elif choice == 4:
+        print("Closing")
+        break
